7acc7f16de57951c267c51046957b50be7fb102b
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-08-26 13:42:05 +0300

    adding to website functionality

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 function Home(){
   return (
     <Card
-      bgcolor="primary"
+      bgcolor="info"
       txtcolor="light"
-      header="BadBank Landing Module"
+      header="BadBank"
       title="Welcome to the bank"
       text="You can move around using the navigation bar."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
