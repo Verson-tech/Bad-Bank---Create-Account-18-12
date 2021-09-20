24fda7bb5654dfe9d7ce2bbf29004996ce7a0d73
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-09-20 15:50:39 +0300

    allData

--- alldata.js
+++ alldata.js
@@ -1,14 +1,14 @@
-function AllData(){
+function AllData(props){              //all data props???                   
 
     return (
         <Card
           bgcolor="info"
           txtcolor="light"
-          header="BadBank All DATA"
-          title="CURRENTLY UNAVAILABLE"
-          text="WORK IN PROGRESS"
+          header={`BadBank All DATA ${props.name}`}
+          // title="CURRENTLY UNAVAILABLE"
+          // text="WORK IN PROGRESS"        
           body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
         />    
       );  
     }
-    
+
