6aa39bf32641bfd71166a4c308873091178dae71
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-09-23 20:24:21 +0300

    zoom call

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function Spa() {
 
   const [userBalance, setBalance]=React.useState(1000)
+  const [allUsers, setAllUsers]= React.useState([])
+  const [currentUser, setCurrentUser] = React.useState()
 
+  function currentUser1(name, email, password, balance){
+    console.log("IN the currentUser function ========")
+    console.log(name)
+    console.log(email)
+    console.log("====================================")
+   // setAllUsers('')
+  // ctx.users.push({ name, email, password, balance });
+  }
 
   return (
     <HashRouter>
       <NavBar/>
-      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:userBalance, setBalance:setBalance}]}}>
+      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:userBalance, setBalance:setBalance, passData:currentUser1}]}}>
         <div className="container" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
