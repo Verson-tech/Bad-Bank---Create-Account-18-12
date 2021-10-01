b7e53063ada0e3b96d8e8a9432ea6dac5114341e
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-01 22:34:50 +0300

    alldata wip

--- alldata.js
+++ alldata.js
@@ -1,6 +1,7 @@
 function AllData(props){              //all data props???                   
 
     return (
+      <div>
         <Card
           bgcolor="info"
           txtcolor="light"
@@ -14,9 +15,28 @@ function AllData(props){              //all data props???
         >                                           
           Get All Data{" "}                      
         </button></>)}
-        />    
+        /> 
+         <Card
+          bgcolor="info"
+          txtcolor="light"
+          header={`BadBank All DATA  ${props.information}`}
+          // title="CURRENTLY UNAVAILABLE"
+          // text="WORK IN PROGRESS"        
+          body={(<> <img src="bank.png" className="img-fluid" alt="Responsive image"/><br /><button
+          type="submit"
+          className="btn btn-light"  
+          onClick={props} 
+        >                                           
+          Get All Data{" "}                      
+        </button></>)}
+        /> 
+        </div>   
       );  
     }
+    
+
+
+
 
     const information = <AllData info="Hello" />;
     ReactDom.render(
--- createaccount.js
+++ createaccount.js
@@ -21,8 +21,8 @@ function CreateAccount() {
     if (!validate(email, "email")) return;
     if (!validate(password.length > 8, "password"))
       return setStatus("Error: password should be at least 8 characters long");
-     console.log(ctx.users)
-    ctx.users[0].passData("ryan", "ryan@gmail.com")
+    console.log(ctx.users);
+    ctx.users[0].passData("ryan", "ryan@gmail.com");
     ctx.users.push({ name, email, password, balance });
     setShow(false);
   }
@@ -36,136 +36,71 @@ function CreateAccount() {
 
   return (
     <div>
-    <Card
-      bgcolor="info"
-      header="Create Account"
-      status={status}
-      body={
-        show ? (
-          <>
-            Name
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="name"
-              placeholder="Enter name"
-              value={name}
-              onChange={(e) => setName(e.currentTarget.value)}
-            />
-            <br />
-            Email address
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            />
-            <br />
-            Password
-            <br />
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="At least 8 characters"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-            />
-            <br />
-            <button
-              type="submit"
-              className="btn btn-light"
-              onClick={handleCreate}
-            >
-              Create Account
-            </button>
-          </>
-        ) : (
-          <>
-            <h5>Success</h5>
-            <button
-              type="submit"
-              className="btn btn-light"
-              data-toggle="tooltip"
-              data-placement="bottom"
-              title="Proceed to adding another account"
-              onClick={clearForm}
-            >
-              Add another account
-            </button>
-          </>
-        )
-      }
-    />
-        <Card
-      bgcolor="info"
-      header="Create Account"
-      status={status}
-      body={
-        show ? (
-          <>
-            Name
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="name"
-              placeholder="Enter name"
-              value={name}
-              onChange={(e) => setName(e.currentTarget.value)}
-            />
-            <br />
-            Email address
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            />
-            <br />
-            Password
-            <br />
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="At least 8 characters"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-            />
-            <br />
-            <button
-              type="submit"
-              className="btn btn-light"
-              onClick={handleCreate}
-            >
-              Create Account
-            </button>
-          </>
-        ) : (
-          <>
-            <h5>Success</h5>
-            <button
-              type="submit"
-              className="btn btn-light"
-              data-toggle="tooltip"
-              data-placement="bottom"
-              title="Proceed to adding another account"
-              onClick={clearForm}
-            >
-              Add another account
-            </button>
-          </>
-        )
-      }
-    />
+      <Card
+        bgcolor="info"
+        header="Create Account"
+        status={status}
+        body={
+          show ? (
+            <>
+              Name
+              <br />
+              <input
+                type="input"
+                className="form-control"
+                id="name"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.currentTarget.value)}
+              />
+              <br />
+              Email address
+              <br />
+              <input
+                type="input"
+                className="form-control"
+                id="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              />
+              <br />
+              Password
+              <br />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="At least 8 characters"
+                value={password}
+                onChange={(e) => setPassword(e.currentTarget.value)}
+              />
+              <br />
+              <button
+                type="submit"
+                className="btn btn-light"
+                onClick={handleCreate}
+              >
+                Create Account
+              </button>
+            </>
+          ) : (
+            <>
+              <h5>Success</h5>
+              <button
+                type="submit"
+                className="btn btn-light"
+                data-toggle="tooltip"
+                data-placement="bottom"
+                title="Proceed to adding another account"
+                onClick={clearForm}
+              >
+                Add another account
+              </button>
+            </>
+          )
+        }
+      />
     </div>
   );
 }
